bdc54575932f7a0898036610c6ff6caad6184bd3
xoth42 <xoth42@protonmail.com>
2022-02-06 21:59:20 -0500

    org

---
+++ src/cpnt.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <cstdio>
+
+//xoth42 Feb 6 base idea of project
+
+using namespace std;
+
+map<string, string> colors = {
+	//basic colors to interpret input
+	{"black",	"30"},
+	{"red",		"31"},
+	{"green",	"32"},
+	{"yellow",	"33"},
+	{"blue",	"34"},
+	{"purple", 	"35"}
+};
+
+int main(int argc, char **argv) 
+{
+	string input = argv[1]; //color pre-interp
+	string colornum;
+
+	try {
+	colornum = colors.at(input); 
+	//color is valid
+	}
+
+	catch (out_of_range error) {
+		cout << "\033[31mcolor is not valid: " << input << endl;
+		return 1;
+	}
+
+	string message = argv[2];
+	for (int i = 2; i < argc; i++) {
+		//add all message contents
+		message = message + " " + argv[i];
+	}
+	string format = "\033["+colornum+"m"; //does the color
+	
+	//done with format, printing
+	cout << format << message << endl;
+	
+}
