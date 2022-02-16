e0db80edebd273c99a552dcea79ad87499721404
xoth42 <xoth42@protonmail.com>
2022-02-16 12:54:00 -0500

    switching computers, unfinished code

--- src/cpnt.cpp
+++ src/cpnt.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+string format = "";
+
 map<string, string> colors = {
 	//basic colors to interpret input
 	{"black",	"30"},
@@ -19,6 +21,25 @@ map<string, string> colors = {
 
 int parse(int argc, char **argv)
 {
+	char str1 = '-';
+	string args = argv[2];
+	if (str1 != argv[2][0]){
+		cout << 0 << endl;
+		return 0;
+	}
+
+	//there is a '-' in the second arg (arguments given)
+	args = args.substr(1, args.length());
+	//'args' is now only the arguments ex) 'ub'
+	char bold = 'b';
+	if (args.find(bold) != std::string::npos){
+		format = "\033[1m";
+	}
+	char italic = 'i';
+	if (args.find(italic) != std::string::npos){
+		format = format + "\e[3m";
+	}
+
 	
 	/*
 	Parse Command line arguments
@@ -30,18 +51,27 @@ int parse(int argc, char **argv)
 	-r		: [rgb value ex=<###,###,###>] [message]...
 	specify rgb color instead of color name
 
-	-b		: [color] [message]...
-	bold
+	
 
 	-u		: [color] [message]...
 	underline
 	*/
-	return 0;
+	return 1;
 }
 
 int main(int argc, char **argv) 
 {
-	string input = argv[1]; //color pre-interp
+	string input = argv[2]; //color pre-interp
+	if (parse(argc, argv) == 1){
+		//bold
+		try{
+		input = argv[3];
+		}
+		catch (out_of_range error){
+			//no more args
+		}
+	}
+	
 	string colornum;
 
 	try {
@@ -59,7 +89,7 @@ int main(int argc, char **argv)
 		//add all message contents
 		message = message + " " + argv[i];
 	}
-	string format = "\033[" + colornum + "m"; //does the color
+	format = format + "\033[" + colornum + "m"; //does the color
 	
 	//done with format, printing
 	cout << format << message << endl;
